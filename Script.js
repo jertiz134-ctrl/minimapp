f20f4aa4c3f0cbda283c6a8e6aa6ec3783695067
jertiz134-ctrl <jertiz.134@gmail.com>
2026-09-19 12:11:49 -0400

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,11 @@
 console.log("Minimapp iniciado correctamente.");
+
+const estado = document.getElementById("status");
+
+setTimeout(() => {
+    estado.textContent = "Inicializando sistema...";
+}, 2000);
+
+setTimeout(() => {
+    estado.textContent = "Minimapp listo.";
+}, 4000);
